1d5f9145c9f7ab194677185b5e6f6464508ddc02
Terrails <github@sl.terrails.com>
2024-05-23 17:17:36 +0200

    gradle update and port of few changes made in backports

--- neoforge/src/main/java/terrails/colorfulhearts/neoforge/compat/UndergardenCompat.java
+++ neoforge/src/main/java/terrails/colorfulhearts/neoforge/compat/UndergardenCompat.java
@@ -1,28 +1,20 @@
 package terrails.colorfulhearts.neoforge.compat;
 
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.client.event.RenderGuiOverlayEvent;
 import net.neoforged.neoforge.common.NeoForge;
-import quek.undergarden.Undergarden;
-import quek.undergarden.registry.UGEffects;
+import org.lwjgl.opengl.GL11;
 import terrails.colorfulhearts.CColorfulHearts;
+import terrails.colorfulhearts.api.heart.drawing.HeartDrawing;
 import terrails.colorfulhearts.api.heart.drawing.SpriteHeartDrawing;
 import terrails.colorfulhearts.api.heart.drawing.StatusEffectHeart;
 import terrails.colorfulhearts.neoforge.api.event.NeoHeartRegistryEvent;
 
 public class UndergardenCompat {
 
-    private static final SpriteHeartDrawing VIRULENCE_SPRITE_DRAWING = SpriteHeartDrawing.build(new ResourceLocation(Undergarden.MODID, "virulence_hearts")).finish(
-            new ResourceLocation(Undergarden.MODID, "virulence_hearts/normal"),
-            new ResourceLocation(Undergarden.MODID, "virulence_hearts/normal_blinking"),
-            new ResourceLocation(Undergarden.MODID, "virulence_hearts/half"),
-            new ResourceLocation(Undergarden.MODID, "virulence_hearts/half_blinking"),
-            new ResourceLocation(Undergarden.MODID, "virulence_hearts/hardcore"),
-            new ResourceLocation(Undergarden.MODID, "virulence_hearts/hardcore_blinking"),
-            new ResourceLocation(Undergarden.MODID, "virulence_hearts/hardcore_half"),
-            new ResourceLocation(Undergarden.MODID, "virulence_hearts/hardcore_half_blinking")
-    );
+    private static final ResourceLocation VIRULENCE_OVERLAY = new ResourceLocation("undergarden", "virulence_hearts");
 
     public UndergardenCompat(IEventBus bus) {
         NeoForge.EVENT_BUS.addListener(this::cancelOverlay);
@@ -30,16 +22,35 @@ public class UndergardenCompat {
     }
 
     private void cancelOverlay(RenderGuiOverlayEvent.Pre event) {
-        if (event.getOverlay().id().equals(new ResourceLocation(Undergarden.MODID, "virulence_hearts"))) {
+        if (event.getOverlay().id().equals(VIRULENCE_OVERLAY)) {
             event.setCanceled(true);
         }
     }
 
     public void registerEffectHeart(final NeoHeartRegistryEvent event) {
-        CColorfulHearts.LOGGER.info("Registering custom hearts for virulence from mod undergarden");
-        event.registerStatusEffectHeart(StatusEffectHeart.build(new ResourceLocation(CColorfulHearts.MOD_ID, "virulence"), player -> player.hasEffect(UGEffects.VIRULENCE.get()))
-                .addHealth(VIRULENCE_SPRITE_DRAWING, 0.6f, 0.6f, 0.6f).addAbsorption(VIRULENCE_SPRITE_DRAWING, 0.6f, 0.6f, 0.6f).finish()
-        );
-        CColorfulHearts.LOGGER.debug("Registered custom hearts for virulence from mod undergarden");
+        BuiltInRegistries.MOB_EFFECT.getOptional(new ResourceLocation("undergarden", "virulence")).ifPresent(effect -> {
+            CColorfulHearts.LOGGER.info("Registering custom hearts for virulence from mod undergarden");
+
+            final ResourceLocation heartId = new ResourceLocation(CColorfulHearts.MOD_ID, "virulence_vanilla");
+            HeartDrawing vanilla = SpriteHeartDrawing.build(new ResourceLocation("undergarden", "virulence_hearts")).finish(
+                    new ResourceLocation("undergarden", "virulence_hearts/normal"),
+                    new ResourceLocation("undergarden", "virulence_hearts/normal_blinking"),
+                    new ResourceLocation("undergarden", "virulence_hearts/half"),
+                    new ResourceLocation("undergarden", "virulence_hearts/half_blinking"),
+                    new ResourceLocation("undergarden", "virulence_hearts/hardcore"),
+                    new ResourceLocation("undergarden", "virulence_hearts/hardcore_blinking"),
+                    new ResourceLocation("undergarden", "virulence_hearts/hardcore_half"),
+                    new ResourceLocation("undergarden", "virulence_hearts/hardcore_half_blinking")
+            );
+
+            event.registerStatusEffectHeart(StatusEffectHeart.build(new ResourceLocation(CColorfulHearts.MOD_ID, "virulence"), player -> player.hasEffect(effect))
+                    .addHealth(vanilla, 0.45f, 0.4f, 0.4f)
+                    .addAbsorption(
+                            HeartDrawing.colorBlend(vanilla, heartId.withSuffix("_absorption"), 1.0f, 1.0f, 1.0f, 0.15f, GL11.GL_ONE, GL11.GL_ONE_MINUS_SRC_ALPHA),
+                            HeartDrawing.colorBlend(vanilla, heartId.withSuffix("_absorption_2"), 1.0f, 0.85f, 0.85f, 0.5f, GL11.GL_ONE, GL11.GL_ONE_MINUS_SRC_ALPHA)
+                    ).finish()
+            );
+            CColorfulHearts.LOGGER.debug("Registered custom hearts for virulence from mod undergarden");
+        });
     }
 }
