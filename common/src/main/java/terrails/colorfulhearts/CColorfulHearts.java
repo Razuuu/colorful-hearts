3dcc922a416e45b7acd07a3fee385746763c1d9e
Terrails <github@sl.terrails.com>
2024-07-09 16:44:28 +0200

    replace ExpectPlatform with regular proxy style classes - ExpectPlatform is failing to randomly get applied on build

--- common/src/main/java/terrails/colorfulhearts/CColorfulHearts.java
+++ common/src/main/java/terrails/colorfulhearts/CColorfulHearts.java
@@ -9,10 +9,15 @@ public class CColorfulHearts {
     public static final String MOD_ID = "colorfulhearts";
     public static final String MOD_NAME = "Colorful Hearts";
     public static final Logger LOGGER = LogManager.getLogger(MOD_NAME);
+    public static PlatformProxy PROXY;
 
     public static final ResourceLocation SPRITE_NAME = new ResourceLocation(CColorfulHearts.MOD_ID, "colored_hearts");
 
     public static ResourceLocation location(String name) {
         return new ResourceLocation(MOD_ID, name);
+    }
+
+    public static void setup(PlatformProxy proxy) {
+        PROXY = proxy;
     }
 }
