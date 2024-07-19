ea7a3abd91b7971e36cccac3d681d9108d4074d3
Terrails <github@sl.terrails.com>
2024-07-19 11:34:33 +0200

    use a common instance of color serializer and deserializer

--- common/src/main/java/terrails/colorfulhearts/config/Configuration.java
+++ common/src/main/java/terrails/colorfulhearts/config/Configuration.java
@@ -2,11 +2,15 @@ package terrails.colorfulhearts.config;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class Configuration {
 
     private static final Predicate<Object> HEX_COLOR_STRING = (o) -> o instanceof String str && str.matches("#[A-Fa-f0-9]{6}");
+    private static final Function<List<String>, List<Integer>> COLOR_DESERIALIZER = strings -> strings.stream().map(Integer::decode).toList();
+    private static final Function<List<Integer>, List<String>> COLOR_SERIALIZER = integers -> integers.stream().map(val -> "#" + Integer.toHexString(val).toUpperCase(Locale.ROOT)).toList();
 
     public static final Health HEALTH = new Health();
     public static final Absorption ABSORPTION = new Absorption();
@@ -28,9 +32,7 @@ public class Configuration {
                         "#F06E14", "#F5DC23", "#2DB928", "#1EAFBE", "#7346E1",
                         "#FA7DEB", "#EB375A", "#FF8278", "#AAFFFA", "#EBEBFF"
                 ),
-                integers -> integers.stream().map(val -> "#" + Integer.toHexString(val)).toList(),
-                strings -> strings.stream().map(Integer::decode).toList(),
-                HEX_COLOR_STRING
+                COLOR_SERIALIZER, COLOR_DESERIALIZER, HEX_COLOR_STRING
         );
         public final ConfigOption<List<String>, List<Integer>> poisonedColors = new ConfigOption<>(
                 "health.poisonedColors",
@@ -38,9 +40,7 @@ public class Configuration {
                         Two alternating colors while poisoned
                         There can be one color in case vanilla poisoned heart is wanted""",
                 List.of("#739B00"),
-                integers -> integers.stream().map(val -> "#" + Integer.toHexString(val)).toList(),
-                strings -> strings.stream().map(Integer::decode).toList(),
-                HEX_COLOR_STRING
+                COLOR_SERIALIZER, COLOR_DESERIALIZER, HEX_COLOR_STRING
         );
         public final ConfigOption<List<String>, List<Integer>> witheredColors = new ConfigOption<>(
                 "health.witheredColors",
@@ -48,9 +48,7 @@ public class Configuration {
                         Two alternating colors while withered
                         There can be one color in case vanilla withered heart is wanted""",
                 List.of("#0F0F0F"),
-                integers -> integers.stream().map(val -> "#" + Integer.toHexString(val)).toList(),
-                strings -> strings.stream().map(Integer::decode).toList(),
-                HEX_COLOR_STRING
+                COLOR_SERIALIZER, COLOR_DESERIALIZER, HEX_COLOR_STRING
         );
         public final ConfigOption<List<String>, List<Integer>> frozenColors = new ConfigOption<>(
                 "health.frozenColors",
@@ -58,9 +56,7 @@ public class Configuration {
                         Two alternating colors while freezing
                         There can be one color in case vanilla frozen heart is wanted""",
                 List.of("#3E70E6"),
-                integers -> integers.stream().map(val -> "#" + Integer.toHexString(val)).toList(),
-                strings -> strings.stream().map(Integer::decode).toList(),
-                HEX_COLOR_STRING
+                COLOR_SERIALIZER, COLOR_DESERIALIZER, HEX_COLOR_STRING
         );
     }
 
@@ -81,33 +77,25 @@ public class Configuration {
                         "#E1FA9B", "#A0FFAF", "#AAFFFA", "#AACDFF", "#D7B4FF",
                         "#FAA5FF", "#FFB4B4", "#FFAA7D", "#D7F0FF", "#EBFFFA"
                 ),
-                integers -> integers.stream().map(val -> "#" + Integer.toHexString(val)).toList(),
-                strings -> strings.stream().map(Integer::decode).toList(),
-                HEX_COLOR_STRING
+                COLOR_SERIALIZER, COLOR_DESERIALIZER, HEX_COLOR_STRING
         );
         public final ConfigOption<List<String>, List<Integer>> poisonedColors = new ConfigOption<>(
                 "absorption.poisonedColors",
                 "Two alternating colors while poisoned",
                 Arrays.asList("#BFF230", "#7AA15A"),
-                integers -> integers.stream().map(val -> "#" + Integer.toHexString(val)).toList(),
-                strings -> strings.stream().map(Integer::decode).toList(),
-                HEX_COLOR_STRING
+                COLOR_SERIALIZER, COLOR_DESERIALIZER, HEX_COLOR_STRING
         );
         public final ConfigOption<List<String>, List<Integer>> witheredColors = new ConfigOption<>(
                 "absorption.witheredColors",
                 "Two alternating colors while withered",
                 Arrays.asList("#787061", "#73625C"),
-                integers -> integers.stream().map(val -> "#" + Integer.toHexString(val)).toList(),
-                strings -> strings.stream().map(Integer::decode).toList(),
-                HEX_COLOR_STRING
+                COLOR_SERIALIZER, COLOR_DESERIALIZER, HEX_COLOR_STRING
         );
         public final ConfigOption<List<String>, List<Integer>> frozenColors = new ConfigOption<>(
                 "absorption.frozenColors",
                 "Two alternating colors while freezing",
                 Arrays.asList("#90D136", "#36D183"),
-                integers -> integers.stream().map(val -> "#" + Integer.toHexString(val)).toList(),
-                strings -> strings.stream().map(Integer::decode).toList(),
-                HEX_COLOR_STRING
+                COLOR_SERIALIZER, COLOR_DESERIALIZER, HEX_COLOR_STRING
         );
     }
 
