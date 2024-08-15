20149239b62251f15fd60d1329164c8963e4f69f
Terrails <github@sl.terrails.com>
2024-08-15 14:53:11 +0200

    make mixin fail hard if it cannot be applied

--- common/src/main/java/terrails/colorfulhearts/mixin/PlayerTabOverlayMixin.java
+++ common/src/main/java/terrails/colorfulhearts/mixin/PlayerTabOverlayMixin.java
@@ -17,9 +17,8 @@ public class PlayerTabOverlayMixin {
     @Inject(method = "renderTablistHearts", cancellable = true,
             at = @At(value = "INVOKE",
                     target = "Lnet/minecraft/client/gui/GuiGraphics;blit(Lnet/minecraft/resources/ResourceLocation;IIIIII)V",
-                    opcode = 0,
-                    shift = At.Shift.BEFORE),
-            locals = LocalCapture.CAPTURE_FAILEXCEPTION)
+                    opcode = 0),
+            locals = LocalCapture.CAPTURE_FAILHARD)
     private void colorfulhearts_renderTablistHearts(int y, int x, int offset, UUID playerUuid, GuiGraphics guiGraphics, int scoreValue, CallbackInfo ci, PlayerTabOverlay.HealthState healthState) {
         // this handles just 2 rows and then uses vanilla NNhp format (default behaviour)
         TabHeartRenderer.INSTANCE.renderPlayerListHud(y, x, offset, guiGraphics, scoreValue, healthState);
