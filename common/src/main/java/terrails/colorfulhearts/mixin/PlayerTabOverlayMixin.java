e890e2f78bbf7a567783f51aecebd83366f002f3
Terrails <github@sl.terrails.com>
2024-08-15 14:36:54 +0200

    use MixinExtras to capture local variable

--- common/src/main/java/terrails/colorfulhearts/mixin/PlayerTabOverlayMixin.java
+++ common/src/main/java/terrails/colorfulhearts/mixin/PlayerTabOverlayMixin.java
@@ -1,12 +1,12 @@
 package terrails.colorfulhearts.mixin;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.PlayerTabOverlay;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 import terrails.colorfulhearts.render.TabHeartRenderer;
 
 import java.util.UUID;
@@ -17,12 +17,11 @@ public class PlayerTabOverlayMixin {
     @Inject(method = "renderTablistHearts", cancellable = true,
             at = @At(value = "INVOKE",
                     target = "Lnet/minecraft/client/gui/GuiGraphics;blitSprite(Lnet/minecraft/resources/ResourceLocation;IIII)V",
-                    opcode = 0,
-                    shift = At.Shift.BEFORE),
-            locals = LocalCapture.CAPTURE_FAILEXCEPTION)
+                    opcode = 0)
+    )
     private void colorfulhearts_renderTablistHearts(
             int y, int x, int offset, UUID playerUuid, GuiGraphics guiGraphics, int scoreValue,
-            CallbackInfo ci, PlayerTabOverlay.HealthState healthState
+            CallbackInfo ci, @Local PlayerTabOverlay.HealthState healthState
     ) {
         // this handles just 2 rows and then uses vanilla NNhp format (default behaviour)
         TabHeartRenderer.INSTANCE.renderPlayerListHud(y, x, offset, guiGraphics, scoreValue, healthState);
